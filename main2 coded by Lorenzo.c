7cb7a0645b481dc70325ecc3e5ea403fa6b788bf
Lorybat1998UCD <Lorenzo.battilocchi@ucdconnect.ie>
2017-02-25 23:27:23 +0000

    Summary of player capabilities  + began struct statement
    Player stats

--- main2 coded by Lorenzo.c
+++ main2 coded by Lorenzo.c
@@ -3,20 +3,24 @@
 int main(){
 	int i, k;
 	int n, max=6, min=2;
+	
+	//prompt user to enter the number of players they will enter
 	printf("Enter the amount of players you will enter below: \n");
-
 	scanf("%d", &n);
+	
+//check that the number of players to be entered abides to the rules of the game
 	if (n>max){
 		printf("Error: too many players. Choose 6 or less players.\n");
 	}
 	else if (n<min){
 		printf("Error: too few players. Choose 2 or more players.\n");
 	}
-
-
-	else{
+//double check array initialisation
+//when the rules are abided, proceed to take in the parameters from the user
+	char array[n][2] = {0}; //inititalise array
+	if (n>=min && n<=max){
 		printf("Enter the players: \n");
-		int array[n][2] = {0}; //inititalise array
+		char array[n][2] = {0}; //inititalise array
 		for (i = 0; i < n; i++){ //nested for loops to fill array
    			for (k = 0; k < 2; k++){
       			scanf("%d", &array [i][k]);	
@@ -24,5 +28,16 @@ int main(){
    			}
    		}
    	
+   	for (i=0; i<n; i++){
+   		for (k=0; k<2; k++){
+   			printf("%s", array[i][k]);
+   		}
+   	}
+
+   	struct Player {
+   		char  name[50];
+   		char  type[50];
+   	}
+   	book;  
    	return 0;
 }
